181674aba8944c602b3fb0aee24692d8d0f346f3
git-chobo <01ypcho@gmail.com>
2023-09-18 15:13:59 +0900

    Var, let, func Study

---
+++ swift_Grammar.playground/Contents.swift
@@ -0,0 +1,27 @@
+var name = "whyP"
+var age = 33
+
+name
+name = "whyP cho"
+
+age
+age = 58
+
+
+func sayHello(name: String) -> String {
+    
+    return "Hello, \(name)"
+}
+
+sayHello(name: "영희")
+
+
+let one = 1
+let two = 2
+
+func add(a: Int, b: Int)->Int {
+    return a+b
+}
+
+add(a:1, b:4)
+
